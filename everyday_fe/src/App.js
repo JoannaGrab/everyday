b326578d87a9255c53b8b74c8e7276e2d4171eae
JoannaGrab <huhu@mail.com>
2021-11-01 19:23:58 +0100

    init frontend

---
+++ everyday_fe/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  const code = 404;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>{code}</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
